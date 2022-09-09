cdb65ead5b63c827dc92b526f667c77092be3fe6
Vitaly Klepachevskiy <vitaly.klepachevskiy@gmail.com>
2022-09-09 13:30:33 +0300

    made changes in master

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 console.log("Hello world");
 console.log("Error");
+
+const age = 35;
+
+if (age >= 18) {
+  console.log("Вы совершеннолетний");
+} else {
+  console.log("Вы несовершеннолетний");
+}
